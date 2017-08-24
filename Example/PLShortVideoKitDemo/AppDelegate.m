9e21ba957288971fba58baa3fd164402be67db40
Siyu Wang <cn.wangsiyu@gmail.com>
2017-08-24 14:27:27 +0800

    delete fabric

--- Example/PLShortVideoKitDemo/AppDelegate.m
+++ Example/PLShortVideoKitDemo/AppDelegate.m
@@ -7,8 +7,6 @@
 //
 
 #import "AppDelegate.h"
-#import <Fabric/Fabric.h>
-#import <Crashlytics/Crashlytics.h>
 
 @interface AppDelegate ()
 
@@ -19,7 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    [Fabric with:@[[Crashlytics class]]];
     return YES;
 }
 
